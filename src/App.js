19059c0ef8fcf226455bc5fbc68ffd1a1db6ce73
Aline Vianna <aline@alinevianna.com.br>
2017-08-20 13:50:45 -0300

    Add fields validation and firebase data model

--- src/App.js
+++ src/App.js
@@ -1,44 +1,113 @@
 import React, { Component } from "react";
 import fire from "./fire";
+import { FormErrors } from "./FormErrors";
+import logo from "./logo-vesto.svg";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { messages: [] }; // <- set up react state
+    this.state = {
+      messages: [],
+      email: "",
+      name: "",
+      formErrors: { email: "", name: "" },
+      emailValid: false,
+      nameValid: false,
+      formValid: false
+    };
   }
-  componentWillMount() {
-    /* Create reference to messages in Firebase Database */
-    let messagesRef = fire
-      .database()
-      .ref("messages")
-      .orderByKey()
-      .limitToLast(100);
-    messagesRef.on("child_added", snapshot => {
-      /* Update React state when message is added at Firebase Database */
-      let message = { text: snapshot.val(), id: snapshot.key };
-      this.setState({ messages: [message].concat(this.state.messages) });
+
+  handleUserInput(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value }, () => {
+      this.validateField(name, value);
     });
   }
-  addMessage(e) {
-    e.preventDefault(); // <- prevent form submit from reloading the page
-    /* Send the message to Firebase */
-    fire.database().ref("messages").push(this.inputEl.value);
-    this.inputEl.value = ""; // <- clear the input
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let nameValid = this.state.nameValid;
+
+    switch (fieldName) {
+      case "email":
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? "" : " é inválido";
+        break;
+      case "name":
+        nameValid = value.length > 2;
+        fieldValidationErrors.name = nameValid ? "" : " é muito curto";
+        break;
+      default:
+        break;
+    }
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        emailValid: emailValid,
+        nameValid: nameValid
+      },
+      this.validateForm
+    );
   }
+
+  validateForm() {
+    this.setState({
+      formValid: this.state.emailValid && this.state.nameValid
+    });
+  }
+
+  addMessage(e) {
+    e.preventDefault();
+    this.inputElEmail.value;
+
+    /* Send the message to Firebase */
+    fire.database().ref("messages").push({
+      name: this.inputElName.value,
+      email: this.inputElEmail.value
+    });
+    this.inputElName.value = "";
+    this.inputElEmail.value = "";
+  }
+
   render() {
     return (
-      <form onSubmit={this.addMessage.bind(this)}>
-        <input type="text" ref={el => (this.inputEl = el)} />
-        <input type="submit" />
-        <ul>
-          {/* Render the list of messages */
-          this.state.messages.map(message =>
-            <li key={message.id}>
-              {message.text}
-            </li>
-          )}
-        </ul>
-      </form>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="Logo Vestô" />
+        </header>
+        <p>Descubra uma nova forma de se vestir! </p>
+        <p>Cadastre-se e receba novidades Vestô.</p>
+        <p>Consuma consciente, consuma diferente.</p>
+        <form onSubmit={this.addMessage.bind(this)}>
+          <div className="panel panel-default">
+            <FormErrors formErrors={this.state.formErrors} />
+          </div>
+          <label htmlFor="completeName">Nome Completo</label>
+          <input
+            id="completeName"
+            type="text"
+            placeholder="Nome Completo"
+            ref={el => (this.inputElName = el)}
+            value={this.state.name}
+            onChange={event => this.handleUserInput(event)}
+            name="name"
+          />
+          <label htmlFor="completeEmail">Email</label>
+          <input
+            id="completeEmail"
+            type="text"
+            placeholder="Email"
+            ref={el => (this.inputElEmail = el)}
+            value={this.state.email}
+            onChange={event => this.handleUserInput(event)}
+            name="email"
+          />
+          <input disabled={!this.state.formValid} type="submit" />
+        </form>
+      </div>
     );
   }
 }

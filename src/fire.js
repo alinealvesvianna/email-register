274447d38a4cacff833a88bd7ec8505373bed1b9
Aline Vianna <aline@alinevianna.com.br>
2017-08-21 00:49:20 -0300

    Add deploy configurations

--- src/fire.js
+++ src/fire.js
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 var config = {
   apiKey: "AIzaSyDHLfOmlOWfL5h3T0WOh9YLSo3lebV7RmY",
-  authDomain: "form-register.firebaseapp.com",
+  authDomain: "vestobrasil.com",
   databaseURL: "https://form-register.firebaseio.com",
   projectId: "form-register",
   storageBucket: "form-register.appspot.com",
